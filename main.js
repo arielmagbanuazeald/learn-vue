956131ef8fc382a0fc4d553afd2baf6d23356d53
Ariel Magbanua <ariel.magbanua@zeald.com>
2019-01-30 10:48:21 +0800

    Component tabs.

--- main.js
+++ main.js
@@ -1,80 +1,65 @@
-Vue.component('task', {
-    template: '<li><slot></slot></li>'
-});
-
-Vue.component('task-list', {
-    template: ` <div>
-                    <task v-for="task in tasks">{{ task.task }}</task>
-                </div>
-                `,
-
-    data() {
-        return {
-            tasks: [
-                { task: 'Go to the store', complete: true },
-                { task: 'Go to the mall', complete: true },
-                { task: 'Go to the farm', complete: true },
-                { task: 'Go to the house', complete: true },
-            ]
-        }
-    }
-});
-
-Vue.component('message', {
-    props: ['title', 'body'],
-
-    data(){
-        return {
-            isVisible: true
-        }
-    },
-
+Vue.component('tabs', {
     template: `
-        <article class="message" v-show="isVisible">
-            <div class="message-header">
-                <p>{{ title }}</p>
-                <button @click="hideModal" class="delete" aria-label="delete"></button>
+        <div>
+            <div class="tabs">
+                <ul>
+                    <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+                        <a :href="tab.href" @click="selectedTab(tab)">{{ tab.name }}</a>
+                    </li>
+                </ul>
             </div>
-            <div class="message-body">
-                {{ body }}
-            </div>
-        </article>
-    `,
 
-    methods: {
-        hideModal(){
-            this.isVisible = !this.isVisible;
-        }
-    }
-});
-
-Vue.component('modal', {
-    props: [],
-    template: `
-        <div class="modal is-active">
-            <div class="modal-background"></div>
-            <div class="modal-card">
-                <header class="modal-card-head">
-                    <p class="modal-card-title"></p>
-                    <button class="delete" aria-label="close" @click="$emit('close')"></button>
-                </header>
-                <section class="modal-card-body">
-                    <slot></slot>
-                </section>
-                <footer class="modal-card-foot">
-                    <button class="button is-success">Save changes</button>
-                    <button class="button" @click="$emit('close')">Cancel</button>
-                </footer>
+            <div class="tabs-details">
+                <slot></slot>
             </div>
         </div>
     `,
+    data(){
+        return {
+            tabs: []
+        };
+    },
 
+    created() {
+        this.tabs = this.$children
+    },
+
+    methods: {
+        selectedTab(selectedTab){
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.name == selectedTab.name)
+            });
+        }
+    }
+});
+
+Vue.component('tab', {
+    props: {
+        name: { required: true },
+        selected: { default: false }
+    },
+
+    template: `
+        <div v-show="isActive"><slot></slot></div>
+    `,
+
+    data(){
+        return {
+            isActive: false
+        };
+    },
+
+    computed: {
+        href(){
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    },
+
+    mounted(){
+        this.isActive = this.selected
+    }
 });
 
 new Vue({
-    el: '#root',
-
-    data: {
-        showModal: false
-    }
+    el: '#root'
 });
