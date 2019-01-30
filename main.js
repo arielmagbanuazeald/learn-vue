cfda0def0a70e289dcba7021647f7dfc53a0bdc2
Dash10-CPU009 <ariel@zeald>
2019-01-30 11:09:52 +0800

    Custom events.

--- main.js
+++ main.js
@@ -1,65 +1,20 @@
-Vue.component('tabs', {
+Vue.component('coupon', {
     template: `
-        <div>
-            <div class="tabs">
-                <ul>
-                    <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
-                        <a :href="tab.href" @click="selectedTab(tab)">{{ tab.name }}</a>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="tabs-details">
-                <slot></slot>
-            </div>
-        </div>
+        <input placeholder="Enter your coupon code" @blur="onCouponApplied">
     `,
-    data(){
-        return {
-            tabs: []
-        };
-    },
-
-    created() {
-        this.tabs = this.$children
-    },
 
     methods: {
-        selectedTab(selectedTab){
-            this.tabs.forEach(tab => {
-                tab.isActive = (tab.name == selectedTab.name)
-            });
+        onCouponApplied(){
+            this.$emit('applied');
         }
-    }
-});
-
-Vue.component('tab', {
-    props: {
-        name: { required: true },
-        selected: { default: false }
-    },
-
-    template: `
-        <div v-show="isActive"><slot></slot></div>
-    `,
-
-    data(){
-        return {
-            isActive: false
-        };
-    },
-
-    computed: {
-        href(){
-            return '#' + this.name.toLowerCase().replace(/ /g, '-');
-        }
-    },
-
-    mounted(){
-        this.isActive = this.selected
     }
 });
 
 new Vue({
-    el: '#root'
+    el: '#root',
+    methods: {
+        onCouponApplied() {
+            alert('It was applied!');
+        }
+    }
 });
